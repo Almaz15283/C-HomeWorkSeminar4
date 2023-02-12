0068ad26bd0dcfa26eb97ff0a7e6f11fd85d9785
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-12 12:23:39 +0300

    Задача найти максимум из 3-х чисел

---
+++ C# Seminar 1/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b && a > c )
+{
+    Console.WriteLine($"Число 1 {a} самое большое");
+    return;
+}
+if (b > a && b > c )
+{
+    Console.WriteLine($"Число 2 {b} самое большое");
+    return;
+}
+if (c > a && c > b )
+{
+    Console.WriteLine($"Число 3 {c} самое большое");
+    return;
+}
