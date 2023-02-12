20273cfcb2bc1cd414a764fa80891a5a257e277a
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-12 23:33:43 +0300

    задача №15

---
+++ C#Seminar 2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// 
+Console.Clear();
+Console.Write("введите число от 1 до 7: ");
+int a = int.Parse(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("ДА! выходной");
+}
+else 
+{
+    Console.WriteLine("НЕТ! Будний день");
+}
