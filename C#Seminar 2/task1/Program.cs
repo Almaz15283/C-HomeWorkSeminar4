d416228b90b927b8fb3cfc6be9f06c12aa994662
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-12 23:10:03 +0300

    решение задачи №10

---
+++ C#Seminar 2/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на 
+//вход трёхзначное число и на выходе показывает 
+//вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Enter your number :");
+int a = int.Parse(Console.ReadLine());
+int a1 = a%100;
+int a2 = a1/10;
+Console.WriteLine(a2);
+
