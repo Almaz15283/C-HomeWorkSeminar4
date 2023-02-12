fcb1e6c6e3d9ffde9cc995cec37bf7745b2bba42
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-12 12:03:15 +0300

    задача на ввод 2 чисел и выявления большего из них

---
+++ C# Seminar 1/task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите 1 число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b) Console.WriteLine($" Число {a} > {b}");
+    else Console.WriteLine($" Число {b} > {a}");
+return;
