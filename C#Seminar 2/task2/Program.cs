f71b929e5d7b57630187fe3919c528420fa5e2f1
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-12 23:28:34 +0300

    задача №13

---
+++ C#Seminar 2/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+Console.Clear();
+Console.Write("Enter your number :");
+int a = int.Parse(Console.ReadLine());
+if (a<100 || a>999) 
+{   
+    Console.WriteLine("Enter correct number!");
+    return;
+}
+    else
+    {
+        int a1 = a%10;
+        Console.WriteLine($"number = {a1}");
+    }
