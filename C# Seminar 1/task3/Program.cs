7c9082c53ebf161835b880e0a4d48513bb26684a
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-12 12:36:06 +0300

    задача на определение четности числа

---
+++ C# Seminar 1/task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("введите число :");
+int a = int.Parse(Console.ReadLine());
+if ( a % 2 == 0 ) Console.WriteLine($" Число {a} четное");
+    else Console.WriteLine($"Число {a} НЕ четное");
+return;
+
+    
