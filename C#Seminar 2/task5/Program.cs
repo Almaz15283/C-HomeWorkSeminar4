3fa7eea7ec0de72080914560c2d599f496aa017a
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-16 22:01:23 +0300

    deBugЭ

--- C#Seminar 2/task5/Program.cs
+++ C#Seminar 2/task5/Program.cs
@@ -6,17 +6,15 @@ int [] clas = new int[n];
 int [] clas1 = new int[r];
 Console.Write("Введите рост Пети: ");
 int petia = int.Parse(Console.ReadLine());
-int p = n;
 for (int i = 0; i < n; i++)
     {
         clas[i] = new Random().Next(100,190);
         clas1[i] = clas[i];
         clas1[r-1] = petia;
     }
-for ( int i = 0; i < r; i++)
+for ( int i = 0; i < (r-1); i++)
     {   
-
-        for( int j = 0; j < (n - i); j++)
+        for ( int j = 0; j < (n - i); j++)
         {
             if (clas1[j] > clas1[j+1]) 
             {
@@ -32,6 +30,7 @@ for (int i =0; i < r; i++)
     if (clas1[i] == petia) 
     {
         Console.WriteLine($"петя будет {i} в строю");
-        Console.Write($"{clas1[i]}, ");
     }
+        Console.Write($"{clas1[i]}, ");
+    
 }
