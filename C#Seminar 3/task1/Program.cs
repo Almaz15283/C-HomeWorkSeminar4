cf67def87173d3326641f29b741979cd0018f6ce
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-19 09:14:13 +0300

    Задача 19

---
+++ C#Seminar 3/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int a = int.Parse(Console.ReadLine());
+/*int a1 = a/10000;
+int a2 = a%10;
+int a3 = a/1000%10;
+int a4 = a%100/10;
+Console.WriteLine(a1);
+Console.WriteLine(a2);
+Console.WriteLine(a3);
+Console.WriteLine(a4);
+Console.WriteLine(a);*/
+if (a > 10000 && a < 100000){
+if (a/10000 == a%10 && a/1000%10 == a%100/10)
+    Console.WriteLine("Число А полиндром");
+    else Console.WriteLine("Число А НЕ полиндром");
+}
+else Console.Write("Введите пятичначное число");
+
