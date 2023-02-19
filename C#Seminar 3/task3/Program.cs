72f84b0763588f63469262e62d5ec94a55cb897a
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-19 09:43:41 +0300

    Задача 23

---
+++ C#Seminar 3/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число N ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    array[i] = i+1;
+    Console.WriteLine($"{Math.Pow(array[i], 3)}");
+}
