7143d230243522691d63d70eae8eecf52ad03e5a
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2023-02-19 09:37:24 +0300

    Задача 21

---
+++ C#Seminar 3/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+int [] A = new int[3];
+int [] B = new int [3];
+double distans = 0;
+for (int i= 0; i < 3; i++)
+{
+    A[i] = new Random().Next(-100,100);
+    B[i] = new Random().Next(-100,100);
+    Console.WriteLine($" A {A[i]}; B {B[i]}");
+}
+distans = Math.Sqrt((Math.Pow((A[0] - B[0]),2) + Math.Pow((A[1] - B[1]), 2) + Math.Pow((A[2] - B[2]), 2)));
+Console.WriteLine($"Расстояние между точками = {Math.Round(distans, 3)}");
